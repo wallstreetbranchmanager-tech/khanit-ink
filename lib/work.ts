a5c7127b0dc50b6a54b9900c10d151fd0581a6e4
wallstreetbranchmanager-tech <wallstreetbranchmanager@gmail.com>
2026-09-10 20:47:51 -0400

    Gallery uses embedded veil plate that landed on GitHub

--- lib/work.ts
+++ lib/work.ts
@@ -1,4 +1,2 @@
 import { item as veil } from "./p_veil";
-import { item as oni } from "./p_oni";
-import { item as sakura } from "./p_sakura";
-export const works = [veil, oni, sakura] as const;
+export const works = [veil] as const;
